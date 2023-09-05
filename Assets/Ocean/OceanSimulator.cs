c1fdf344b976b301a6e9b278850abc2bea19c2e7
PickyDogFish <pifko.kosir@gmail.com>
2023-09-05 15:37:55 +0200

    Time dependent spectrum

--- Assets/Ocean/OceanSimulator.cs
+++ Assets/Ocean/OceanSimulator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,33 +8,57 @@ public class OceanSimulator : MonoBehaviour
 {
 
     [SerializeField] private RenderTexture initialSpectrum;
+    [SerializeField] private RenderTexture timeSpectrum;
     [SerializeField] private OceanParameters oceanParameters;
+    [SerializeField] private ComputeShader spectrumShader;
+
+    private int workGroupsX = 8, workGroupsY = 8;
+
     // Start is called before the first frame update
     void Start()
     {
         initialSpectrum = CreateRenderTexture(oceanParameters.size, 1);
+
+        spectrumShader.SetInt(Shader.PropertyToID("_Size"), oceanParameters.size);
+        spectrumShader.SetFloat(Shader.PropertyToID("_WindSpeed"), oceanParameters.windSpeed.magnitude);
+        spectrumShader.SetFloat(Shader.PropertyToID("_Scale"), oceanParameters.lengthScale);
+        spectrumShader.SetFloat(Shader.PropertyToID("_Depth"), oceanParameters.depth);
+
+        spectrumShader.SetTexture(0, Shader.PropertyToID("_InitialSpectrum"), initialSpectrum);
+        spectrumShader.Dispatch(0, oceanParameters.size / workGroupsX, oceanParameters.size / workGroupsY, 1);
+        spectrumShader.SetTexture(1, Shader.PropertyToID("_InitialSpectrum"), initialSpectrum);
+        spectrumShader.Dispatch(1, oceanParameters.size / workGroupsX, oceanParameters.size / workGroupsY, 1);
+
+
+        //time dependent spectrum
+        timeSpectrum = CreateRenderTexture(oceanParameters.size, 1);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spectrumShader.SetFloat(Shader.PropertyToID("_FrameTime"), Time.time);
+        spectrumShader.SetTexture(2, Shader.PropertyToID("_InitialSpectrum"), initialSpectrum);
+        spectrumShader.SetTexture(2, Shader.PropertyToID("_TimeSpectrum"), timeSpectrum);
+        spectrumShader.Dispatch(2, oceanParameters.size / workGroupsX, oceanParameters.size / workGroupsY, 1);
     }
 
-    RenderTexture CreateRenderTexture(int size, int cascadeNumber){
+    RenderTexture CreateRenderTexture(int size, int cascadeNumber)
+    {
         RenderTextureDescriptor initialsDescriptor = new RenderTextureDescriptor()
-            {
-                height = size,
-                width = size,
-                volumeDepth = cascadeNumber,
-                enableRandomWrite = true,
-                colorFormat = RenderTextureFormat.ARGBHalf,
-                sRGB = false,
-                msaaSamples = 1,
-                depthBufferBits = 0,
-                useMipMap = false,
-                dimension = TextureDimension.Tex2DArray
-            };
+        {
+            height = size,
+            width = size,
+            volumeDepth = cascadeNumber,
+            enableRandomWrite = true,
+            colorFormat = RenderTextureFormat.ARGBHalf,
+            sRGB = false,
+            msaaSamples = 1,
+            depthBufferBits = 0,
+            useMipMap = false,
+            dimension = TextureDimension.Tex2DArray
+        };
         RenderTexture rt = new RenderTexture(initialsDescriptor);
         rt.Create();
         return rt;
