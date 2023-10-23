8fcd4bcc03c3f0036cafa5230824efff8aa0cc33
PickyDogFish <pifko.kosir@gmail.com>
2023-10-23 15:02:00 +0200

    calculate initial spectrum into a texture

--- Assets/Ocean/MathTranslation/MathTranslation.cs
+++ Assets/Ocean/MathTranslation/MathTranslation.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 
 public class MathTranslation : MonoBehaviour
 {
-    [SerializeField] private Transform mainLight;
+    [Header("Initial spectrum settings")]
+    [SerializeField] private ComputeShader spectrumShader;
+    [SerializeField] private RenderTexture initialSpectrumTex;
+    [SerializeField] private Vector2 wind = new Vector2(5,2);
+    [SerializeField] private float phillipsA = 0.1f;
+    
+    
+    
+    [Header("Other settings")]
     [SerializeField] private int texSize = 128;
+    [SerializeField] private float len = 128;
+    [SerializeField] private ComputeShader mathShader;
+
+    
+
+    [Header("Previews")]
     [SerializeField] private RenderTexture heightTex;
     [SerializeField] private RenderTexture normalTex;
     [SerializeField] private RenderTexture displacementTex;
-    [SerializeField] private ComputeShader mathShader;
-    [SerializeField] private float len = 128;
-    [SerializeField] private Vector2 wind = new Vector2(5,2);
-    [SerializeField] private float phillipsA = 0.1f;
     private Texture2D gaussianNoise;
     
     [SerializeField] public Material material;
@@ -30,9 +40,23 @@ public class MathTranslation : MonoBehaviour
         {
             enableRandomWrite = true
         };
+        initialSpectrumTex = new RenderTexture(texSize, texSize, 0, RenderTextureFormat.ARGBFloat)
+        {
+            enableRandomWrite = true
+        };
         
         gaussianNoise = GaussianNoise.GenerateTex(texSize);
+        CalculateInitialSpectrum();
+    }
 
+    void CalculateInitialSpectrum(){
+        spectrumShader.SetTexture(0, Shader.PropertyToID("_NoiseTex"), gaussianNoise);
+        spectrumShader.SetTexture(0, Shader.PropertyToID("_InitialSpectrumTex"), initialSpectrumTex);
+        spectrumShader.SetFloat("_A", phillipsA);
+        spectrumShader.SetInt(Shader.PropertyToID("_Size"), texSize);
+        spectrumShader.SetFloat(Shader.PropertyToID("_Length"), len);
+        spectrumShader.SetVector(Shader.PropertyToID("_Wind"), wind);
+        spectrumShader.Dispatch(0, texSize/8, texSize/8, 1);
     }
 
     // Update is called once per frame
