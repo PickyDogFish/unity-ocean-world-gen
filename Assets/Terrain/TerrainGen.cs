9da5b30b788efaa0de33cc8ac1bae74a1498cf51
PickyDogFish <pifko.kosir@gmail.com>
2023-11-24 15:36:26 +0100

    terrain tiling

--- Assets/Terrain/TerrainGen.cs
+++ Assets/Terrain/TerrainGen.cs
@@ -1,13 +1,16 @@
 
+using System.ComponentModel;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.TerrainUtils;
 
 public class TerrainGen : MonoBehaviour
 {
-    private static Vector2 tileAmount = Vector2.one;
     private ComputeShader noiseCS;
-    private float width = 1000;
-    private float length = 1000;
-    private float height = 600;
+    [SerializeField] private float width = 512;
+    [SerializeField] private float length = 512;
+    [SerializeField] private float height = 256;
+    private Vector3 size {get {return new Vector3(width, height, length);}}
 
     private int heightmapResolution = 513; //apparently has to be one more
     private int detailResolution = 1024;
@@ -15,22 +18,30 @@ public class TerrainGen : MonoBehaviour
     private int controlTextureResolution = 512;
     private int baseTextureResolution = 1024;
 
-    private string path = string.Empty;
-
     void Awake()
     {
         noiseCS = Resources.Load<ComputeShader>("NoiseGenerator");
     }
     void Start()
     {
-        CreateTerrainTile(Vector2Int.zero);
+        Terrain startingTerrain = CreateTerrain(Vector2Int.zero);
+        startingTerrain.SetNeighbors(CreateTerrain(Vector2Int.left), CreateTerrain(Vector2Int.up), CreateTerrain(Vector2Int.right), CreateTerrain(Vector2Int.down));
     }
 
-    private void CreateTerrainTile(Vector2Int tileCoords)
+    
+
+    public Terrain CreateTerrain(Vector2Int terrainCoords){
+        GameObject newTerrainGO = Terrain.CreateTerrainGameObject(CreateTerrainData(terrainCoords));
+        newTerrainGO.name = "Terrain" + terrainCoords.ToString();
+        newTerrainGO.transform.parent = gameObject.transform;
+        Vector3 pos = new Vector3(terrainCoords.x, 0, terrainCoords.y);
+        pos.Scale(size);
+        newTerrainGO.transform.position = pos;
+        return newTerrainGO.GetComponent<Terrain>();
+    }
+
+    private TerrainData CreateTerrainData(Vector2Int tileCoords)
     {
-
-
-
         TerrainData terrainData = new TerrainData();
 
         terrainData.size = new Vector3(width / 16f,
@@ -39,17 +50,14 @@ public class TerrainGen : MonoBehaviour
 
         terrainData.baseMapResolution = baseTextureResolution;
         terrainData.heightmapResolution = heightmapResolution;
-        terrainData.SetHeights(0, 0, NoiseGen.GetNoiseArray(noiseCS, heightmapResolution, 1));
+        terrainData.SetHeights(0, 0, NoiseGen.GetNoiseArray(tileCoords, noiseCS, heightmapResolution, 1));
 
         terrainData.alphamapResolution = controlTextureResolution;
         terrainData.SetDetailResolution(detailResolution, detailResolutionPerPatch);
 
-        terrainData.name = name;
-        GameObject terrain = Terrain.CreateTerrainGameObject(terrainData);
-
-        terrain.name = name;
-        terrain.transform.parent = gameObject.transform;
-        terrain.transform.position = new Vector3(tileCoords.x * width, 0, tileCoords.y * length);
+        terrainData.name = name + tileCoords.ToString();
+        
+        return terrainData;
     }
 
 
