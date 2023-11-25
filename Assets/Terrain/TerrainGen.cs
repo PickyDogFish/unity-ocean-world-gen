cbf404e240a1fb6511ace225d577c186d6d5d0fc
PickyDogFish <pifko.kosir@gmail.com>
2023-11-25 16:42:28 +0100

    painting the terrain

--- Assets/Terrain/TerrainGen.cs
+++ Assets/Terrain/TerrainGen.cs
@@ -2,22 +2,35 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TerrainUtils;
 
 public class TerrainGen : MonoBehaviour
 {
     private ComputeShader noiseCS;
-    [SerializeField] private float width = 512;
-    [SerializeField] private float length = 512;
-    [SerializeField] private float height = 256;
-    private Vector3 size {get {return new Vector3(width, height, length);}}
+
+    [Header("Chunk settings")]
+    [SerializeField] private float widthScale = 512;
+    [SerializeField] private float lengthScale = 512;
+    [SerializeField] private float heightScale = 256;
+    private Vector3 size {get {return new Vector3(widthScale, heightScale, lengthScale);}}
+
+    [SerializeField] private float noiseScale = 1;
+
+
+    [Header("Texture settings")]
+    [SerializeField] TerrainLayer rockLayer;
+    [SerializeField] TerrainLayer grassLayer;
+    [SerializeField] TerrainLayer sandLayer;
 
     private int heightmapResolution = 513; //apparently has to be one more
     private int detailResolution = 1024;
     private int detailResolutionPerPatch = 8;
-    private int controlTextureResolution = 512;
+    private int alphamapResolution = 512;
     private int baseTextureResolution = 1024;
+
+
     [SerializeField] private int tileRange = 1;
 
     private Dictionary<Vector2Int, Terrain> tileDictionary = new Dictionary<Vector2Int, Terrain>();
@@ -35,11 +48,6 @@ public class TerrainGen : MonoBehaviour
                 AddTerrain(new Vector2Int(x,y));
             }
         }
-        //AddTerrain(Vector2Int.zero);
-        //AddTerrain(new Vector2Int(0,1));
-        //AddTerrain(new Vector2Int(0,-1));
-
-        PrintNeighbors(Vector2Int.zero);
     }
 
     public void AddTerrain(Vector2Int terrainCoords){
@@ -69,7 +77,6 @@ public class TerrainGen : MonoBehaviour
             terrain.SetNeighbors(terrain.leftNeighbor, terrain.topNeighbor, terrain.rightNeighbor, bottomTile);
             bottomTile.SetNeighbors(bottomTile.leftNeighbor, terrain, bottomTile.rightNeighbor, bottomTile.bottomNeighbor);
         }
-        PrintNeighbors(terrainCoords);
     }
 
     private void PrintNeighbors(Vector2Int terrainCoords){
@@ -81,9 +88,11 @@ public class TerrainGen : MonoBehaviour
         GameObject newTerrainGO = Terrain.CreateTerrainGameObject(CreateTerrainData(terrainCoords));
         newTerrainGO.name = "Terrain" + terrainCoords.ToString();
         newTerrainGO.transform.parent = gameObject.transform;
+
         Vector3 pos = new Vector3(terrainCoords.x, 0, terrainCoords.y);
         pos.Scale(size);
         newTerrainGO.transform.localPosition = pos;
+
         Terrain terrain = newTerrainGO.GetComponent<Terrain>();
         terrain.groupingID = 1;
         terrain.allowAutoConnect = false;
@@ -94,22 +103,60 @@ public class TerrainGen : MonoBehaviour
     {
         TerrainData terrainData = new TerrainData();
 
-        terrainData.size = new Vector3(width / 16f,
-                                        height,
-                                        length / 16f);
+        terrainData.size = new Vector3(widthScale / 16f,
+                                        heightScale,
+                                        lengthScale / 16f);
 
         terrainData.baseMapResolution = baseTextureResolution;
         terrainData.heightmapResolution = heightmapResolution;
-        terrainData.SetHeights(0, 0, NoiseGen.GetNoiseArray(tileCoords, noiseCS, heightmapResolution, 1));
+        float[,] heights = NoiseGen.GetNoiseArray(tileCoords, noiseCS, heightmapResolution, noiseScale);
+        terrainData.SetHeights(0, 0, heights);
 
-        terrainData.alphamapResolution = controlTextureResolution;
+        terrainData.alphamapResolution = alphamapResolution;
         terrainData.SetDetailResolution(detailResolution, detailResolutionPerPatch);
 
         terrainData.name = name + tileCoords.ToString();
-        
+        terrainData.terrainLayers = new TerrainLayer[3]{rockLayer, grassLayer, sandLayer};
+        terrainData.SetAlphamaps(0,0, CreateAlphaMap(terrainData, heights));
+
         return terrainData;
     }
 
+    private float[,,] CreateAlphaMap(TerrainData terrainData, float[,] heights){
 
+        float[,,] map = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, 3];
 
+        // For each point on the alphamap...
+        for (int y = 0; y < terrainData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < terrainData.alphamapWidth; x++)
+            {
+                // Get the normalized terrain coordinate that
+                // corresponds to the point.
+                float normX = x * 1.0f / (terrainData.alphamapWidth - 1);
+                float normY = y * 1.0f / (terrainData.alphamapHeight - 1);
+
+                // Get the steepness value at the normalized coordinate.
+                float angle = terrainData.GetSteepness(normX, normY);
+                float height = WorldSpaceHeight(heights[x,y]);
+
+                // Steepness is given as an angle, 0..90 degrees. Divide
+                // by 90 to get an alpha blending value in the range 0..1.
+                if (height < 0){
+                    map[x, y, 0] = 0;
+                    map[x, y, 1] = 0;
+                    map[x,y,2] = 1;
+                } else {
+                    float frac = angle / 90.0f;
+                    map[x, y, 0] = frac;
+                    map[x, y, 1] = 1 - frac;
+                }
+            }
+        }
+        return map;
+    }
+
+    private float WorldSpaceHeight(float height){
+        return height * heightScale + transform.position.y;
+    }
 }
